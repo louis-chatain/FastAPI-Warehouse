0225209b49419b08f7542de9375c0855d8efd20f
Louis Chatain <louischatain123@gmail.com>
2025-12-03 12:07:14 +0100

    added redis connection, redis password is in .env and retreived with python-dotenv==1.2.1

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection
-
+from dotenv import load_dotenv
+import os
 app = FastAPI()
 
 origins: list[str] = ["http://localhost:3000", "http://127.0.0.1:3000"]
@@ -15,4 +16,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-redis = get_redis_connection()
+load_dotenv()
+
+redis_password = os.environ.get("REDIS_PASSWORD")
+
+redis = get_redis_connection(
+    host = "redis-11913.c321.us-east-1-2.ec2.cloud.redislabs.com",
+    port = "11913",
+    password = redis_password,
+    decode_responses=True
+)
